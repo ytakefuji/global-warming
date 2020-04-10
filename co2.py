bacfd4253a5a43c85141a745934cfad0a904fa7a
Yoshiyasu Takefuji <ytakefuji@gmail.com>
2020-04-10 15:09:40 +0900

    Add files via upload

--- co2.py
+++ co2.py
@@ -2,20 +2,26 @@ co2=open('co2.txt','rb')
 data=[]
 for i in co2:
  a,b,c,d,e,f,g=i.split()
- data.append(a+'.'+b+','+e)
+ if len(b)==1:
+  data.append(a+'_0'+b+','+e)
+ else:
+  data.append(a+'_'+b+','+e)
 f=open('co2','wb')
 f.write("\n".join(data))
 f.close()
 import matplotlib.pyplot as plt
+import matplotlib.ticker as ticker
 import pandas as pd
 data=pd.read_csv('co2')
 data.columns=['year','co2']
 plt.xlabel('year')
 plt.ylabel('density')
 plt.plot(data['year'],data['co2'],'k.')
+plt.xticks(rotation='vertical',fontsize=8)
 #plt.plot(data['year'],data['co2'],'k')
+#plt.locator_params(axis='x',nbins=10)
 fig=plt.figure(1)
-fig.set_size_inches(10,5)
+fig.set_size_inches(100,50)
 plt.savefig('co2.png',dpi=fig.dpi,bbox_inches='tight')
 plt.show()
 plt.close()
